f41757f8474ecdd6e47bdcfe9eedecc8f995a634
MrsMillerComputing <felicity.miller@ab-ed.org>
2025-07-28 11:30:30 +0100

    Create 3Debug.py

---
+++ 3Debug.py
@@ -0,0 +1,23 @@
+#3. Spelling
+
+# Declare and initialise variables
+pupilName = 'Max Myles'
+age = 15
+school = 'Hawkins High School'
+
+# Prints blank line
+print('\n')
+    
+# Display welcome message
+print('Welcome to' + school)
+print('================================')
+    
+# Prints blank line
+print('\n')
+
+# Display pupil details
+print('Pupil name: ' + pupilname)
+print('Pupil age:  + age')
+
+# Prints blank line
+print('\n')

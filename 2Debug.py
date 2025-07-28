938b357687bf8a0ea0577f22bc3781757921a906
MrsMillerComputing <felicity.miller@ab-ed.org>
2025-07-28 11:29:02 +0100

    Create 2Debug.py

---
+++ 2Debug.py
@@ -0,0 +1,7 @@
+#2. Variable or value
+
+# Assign value to variable
+favourite_Disney_Movie = Big Hero 6
+
+# Display favourite Disney movie
+print(My favourite Disney movie is  + 'favourite-Disney-Movie')

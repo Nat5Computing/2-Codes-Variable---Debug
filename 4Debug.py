d092034c36888334d6401a071b3a578e04b888b7
MrsMillerComputing <felicity.miller@ab-ed.org>
2025-07-28 11:31:06 +0100

    Create 4Debug.py

---
+++ 4Debug.py
@@ -0,0 +1,23 @@
+#4. No errors but wrong output
+
+# declare and initialise variables
+pupilName = 'Max Myles'
+age = 15
+school = 'Hawkins High School'
+
+# prints blank line
+print('\n')
+    
+# display welcome message
+print('Welcome to ' + str(age))
+print('================================')
+    
+# prints blank line
+print('\n')
+
+# display pupil details
+print('Pupil name: ' + school)
+print('Pupil age: ' + pupilName)
+
+# prints blank line
+print('\n')
